ff57ef20c8d0448b975fffa8757a22b7d5b1957c
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-03-26 06:34:10 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,32 +1,45 @@
 # Write your code here.
-def dictionary(sent)
-  str = ''
-  if sent == "hello"
-    str = 'hi'
-  else if sent == "to" || sent == "two" || sent == "too"
-    str = '2' 
-  else if sent == "for" || sent == "four" 
-    str = '4'
-  else if sent == 'be' 
-    str = 'b'
-  else if sent == 'you' 
-    str = 'u'
-  else if sent == "at" 
-    str = "@" 
-  else if sent == "and" b
-    str = "&"
-  end
-  str
-end
+# def dictionary(sent)
+#   str = ''
+#   if sent == "hello"
+#     str = 'hi'
+#   else if sent == "to" || sent == "two" || sent == "too"
+#     str = '2' 
+#   else if sent == "for" || sent == "four" 
+#     str = '4'
+#   else if sent == 'be' 
+#     str = 'b'
+#   else if sent == 'you' 
+#     str = 'u'
+#   else if sent == "at" 
+#     str = "@" 
+#   else if sent == "and"
+#     str = "&"
+#   end
+#   str
+# end
 
 def dictionary
   dictionary = {
     "hello" => 'hi',
-["to", "two", "too"] => '2', 
-["for", "four"] => '4',
+"to" => '2', "two" => "2", "too" => '2', 
+"for" => '4', "four" => '4',
 'be' => 'b',
 'you' => 'u',
 "at" => "@" ,
-"and" => "&",
+"and" => "&"
   }
+end
+
+def word_substituter(sent)
+  str = ""
+  arr = []
+  words = sent.split(" ")
+  i = 0 
+  while i < words.length 
+    word = word[i]
+      if word == dictionary.keys[i]
+    i += 1 
+  end
+  
 end
